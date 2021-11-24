d66db6216ba79b0fbb961de9af9e7e5bbbfcf841
BasToTheMax <bas@arnold-linda.nl>
2021-11-24 12:59:04 +0100

    Connect to database
    with logging

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const logger = require('./lib/logging');
 const apiRoutes = require('./lib/api');
 
 const app = express();
+const DataBase = require('./lib/mysql');
+const db = new DataBase();
 
 // Use api routes
 app.use('/api', apiRoutes);
+
+db.query('SELECT 1 + 1;');
 
 
 // Run the webserver
