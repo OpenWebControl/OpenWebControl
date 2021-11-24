44d47b35a57c3cee7d639b303f9e74e8c30837ed
BasToTheMax <bas@arnold-linda.nl>
2021-11-24 12:49:22 +0100

    Add logging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import packages
 const express = require('express');
 const config = require('./lib/config');
+const logger = require('./lib/logging');
 
 // Import api routes
 const apiRoutes = require('./lib/api');
@@ -13,5 +14,5 @@ app.use('/api', apiRoutes);
 
 // Run the webserver
 app.listen(config.main.port, () => {
-    console.log(`App online at port ${config.main.port}!`);
+    logger.info(`app online at port ${config.main.port}!`);
 });
